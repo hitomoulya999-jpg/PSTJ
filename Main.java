4a5b09ee087574a6be4dfdd1e084576f6970d70d
hitomoulya999-jpg <hitomoulya999@gmail.com>
2026-07-24 15:33:50 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        Scanner sc = new Scanner(System.in);
+
+        int index = sc.nextInt();
+
+        System.out.println(arr[index]);
+    }
+}
---
+++ binary search.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        Scanner sc = new Scanner(System.in);
+
+        int key = sc.nextInt();
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key) {
+                System.out.println("Element found at index " + mid);
+                return;
+            } else if (key < arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        System.out.println("Element not found");
+    }
+}
